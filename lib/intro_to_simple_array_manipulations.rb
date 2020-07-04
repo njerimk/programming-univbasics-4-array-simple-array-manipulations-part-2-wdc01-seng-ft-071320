70842257bbc34049b93ef13a7aa215dc20f8ab73
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-04 19:35:09 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,39 +1,32 @@
 
 def using_concat(my_favorite_things,more_favs)
- 
- @my_favorite_things=[]
- more_favs=["raindrops on roses","whiskers on kittens", "sports cars", "flatiron school"]
-   @my_favorite_things.concat(more_favs)
+   @my_favorite_things=[]
+   @all_my_favs=["raindrops on roses", "whiskers on kittens", "sports cars", "flatiron school"]
+   @new_array=@my_favorite_things.concat(@all_my_favs)
 end
 
-def using_insert(list_of_programming_languages, another_language)
-  @new_array=["ruby","java","javascript","python","SQL","PHP","C#"]
-  @new_array.insert(4,"Python")
+def using_insert(array, another_language)
+  array=["Python","Love", "Java", "JavaScript","SQL", "C#", "CSS"]
+  array.insert(4,"Python")
 end
 
 def using_uniq(array)
-  haircuts=["Pixie", "Bob", "Mohawk","Crew Cut","Linka", "Wheeler"]
-  new_array=haircuts.uniq
+  array=["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler"]
+  array.uniq
 end
 
 def using_flatten(array)
-  instruments=["Saxophone","Piano","Trumpet", "Violin", "Drums","Flute"]
-  flat_array=instruments.flatten
+  array= ["Saxophone", "Piano", "Trumpet", "Violin", "Drums", "Flute"]
+  array.flatten
 end
 
 def using_delete(array,string)
-  instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-  no_offense_steven=instructors.delete("Steven")
-  instructors=instructors.to_s
-  puts instructors
+  instructors=["Josh", "Steven", "Sophie","Steven", "Amanda", "Steven"]
+  sorry_steven="Steven"
+  array.delete(sorry_steven)
 end
-array = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-no_offense_steven=array.delete("Steven")
-string = "Steven"
-using_delete(array,string)
-
 
 def using_delete_at(array,integer)
-  robot=["Robocop"]
-  robot.delete("Robocop")
+  array=["Robocop", "Super Woman"]
+  array.delete_at(0)
 end
